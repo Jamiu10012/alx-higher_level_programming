465a0bdcb9ad81740def241bc13ed0af43179e49
Ibrahim Jamiu <ibrahimjamiu026@gmail.com>
2024-01-08 07:51:15 -0800

    done

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,33 +1,19 @@
 #!/usr/bin/python3
-"""Implementation of the 'rectangle' class that inherits from 'BaseGeometry'
-"""
-
-'''
-Project Title: 0x0A-python-inheritance
-Status: Submitted.
-'''
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    # Write a class Rectangle that inherits from BaseGeometry..
-    # ....(7-base_geometry.py).
-    # Instantiation with width and height: def __init__(self, width, height):
-    # VARIABLE(" "):
-    # Rectangle(BaseGeometry): Rectangle
-    # width and height must be private. No getter or setter
-    # width and height must be positive integers, validated...
-    # ....by integer_validator....
-    # Return: Always 0. (Success)
-    """
-    """
-    The 'Rectangle' class has a constructor '__init__' that takes 'width' and
-    ...'height' as parameters. The width and height are set as private...
-    attributes('_-width' and 'height')within the class. The 'integer_
-    ..validator' method from the 'BaseGeometry' class is called to validate
-    ..the width and height, ensuring they are positive integers"""
+    """Represent a rectangle using BaseGeometry."""
+
     def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
+        """Intialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+        """
         self.integer_validator("width", width)
+        self.__width = width
         self.integer_validator("height", height)
+        self.__height = height
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,46 +1,29 @@
 #!/usr/bin/python3
-"""Updated Implementation of the 'rectangle' class that inherits from 'BaseGeometry'
-"""
-
-'''
-ze: Undefined
-Project Title: 0x0A-python-inheritance
-Status: Submitted.
-'''
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    # Write a class Rectangle that inherits from BaseGeometry..
-    # ....(7-base_geometry.py).(task based on 8-rectangle.py)
-    # Instantiation with width and height: def __init__(self, width, height):
-    # VARIABLE(" "):
-    # Rectangle(BaseGeometry): Full rectangle
-    # width and height must be private. No getter or setter
-    # width and height must be positive integers, validated...
-    # ....by integer_validator....
-    # Return: Always 0. (Success)
-    """
-    """
-    The 'Rectangle' class has a constructor '__init__' that takes 'width' and
-    ...'height' as parameters. The width and height are set as private...
-    attributes('_-width' and 'height')within the class. The 'integer_
-    ..validator' method from the 'BaseGeometry' class is called to validate
-    ..the width and height, ensuring they are positive integers"""
+    """Represent a rectangle using BaseGeometry."""
+
     def __init__(self, width, height):
+        """Intialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+        """
+        super().integer_validator("width", width)
         self.__width = width
+        super().integer_validator("height", height)
         self.__height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
 
     def area(self):
-        """
-        The 'area()' method is implemented to calculate the area of the....
-        rectangle by multiplying its width and height."""
+        """Return the area of the rectangle."""
         return self.__width * self.__height
 
     def __str__(self):
-        """
-        The '__str__' method is overridden to return a string representation
-        of the rectangle in the desired format...."""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        """Return the print() and str() representation of a Rectangle."""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
